317fdc774ba3efe6ae30235bff99c775d3392adc
Eliza Chernysh <82468286+ElizaChernysh@users.noreply.github.com>
2023-01-26 18:07:39 +0200

    added handler

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,16 @@
 import { Router } from 'express';
 import { register, login, getMe } from '../controllers/auth.js';
 import { checkAuth } from '../utils/checkAuth.js';
-import {handleValidationErrors} from '../utils/handleValidationErrors.js';
+import { handleValidationErrors} from '../utils/handleValidationErrors.js';
 import { registerValidation, loginValidation } from '../validations/auth.js';
 
 const router = new Router();
 
 //Register
-router.post('/register', registerValidation, register);
+router.post('/register', registerValidation, handleValidationErrors, register);
 
 //Login
-router.post('/login', loginValidation, login);
+router.post('/login', loginValidation, handleValidationErrors, login);
 
 //Get me
 router.get('/me', checkAuth, getMe);
