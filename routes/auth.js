5d8343ef8d017b57c9d7c6c4280c1609b5e1b124
Eliza Chernysh <82468286+ElizaChernysh@users.noreply.github.com>
2023-01-26 17:57:15 +0200

    added validations

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,16 @@
 import { Router } from 'express';
 import { register, login, getMe } from '../controllers/auth.js';
 import { checkAuth } from '../utils/checkAuth.js';
+import {handleValidationErrors} from '../utils/handleValidationErrors.js';
+import { registerValidation, loginValidation } from '../validations/auth.js';
 
 const router = new Router();
 
 //Register
-router.post('/register', register);
+router.post('/register', registerValidation, register);
 
 //Login
-router.post('/login', login);
+router.post('/login', loginValidation, login);
 
 //Get me
 router.get('/me', checkAuth, getMe);
